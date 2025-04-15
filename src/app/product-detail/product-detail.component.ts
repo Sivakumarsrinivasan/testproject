333c301964ddbadadc66b8699be10964c38ebcd5
Sivakumarsrinivasan <ssivakumar6242001@gmail.com>
2025-04-15 23:50:53 +0530

    Image add

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -6,120 +6,165 @@ import { product } from '../model.component';
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent implements OnInit{
+export class ProductDetailComponent implements OnInit {
   ngOnInit(): void {
-this.assigninglist()
+    this.assigninglist()
   }
   productlist: product[] = [
     {
       price: 100,
-      image_data: 'assets/appliances.jpg',
+      image_data: 'assets/Aline.jpg',
       rating: 3,
       id: 0,
-      name: 'computer',
-      quantity: 1,
+      name: 'Aline dress',
+      quantity: 20,
       category: 1
     },
     {
       price: 200,
-      image_data: 'assets/appliances.jpg',
+      image_data: 'assets/maxi.jpg',
       rating: 2,
       id: 1,
-      name: 'laptop',
-      quantity: 1,
+      name: 'Maxi',
+      quantity: 35,
       category: 1
     },
     {
       price: 300,
-      image_data: 'assets/appliances.jpg',
+      image_data: 'assets/bodycon.jpg',
       rating: 3,
       id: 2,
-      name: 'phone',
-      quantity: 1,
+      name: 'Bodycon',
+      quantity: 40,
       category: 1
     },
     {
       price: 400,
-      image_data: 'assets/appliances.jpg',
+      image_data: 'assets/tuxedo.jpg',
       rating: 3,
       id: 3,
-      name: 'Tv',
-      quantity: 1,
+      name: 'Tuxedo',
+      quantity: 15,
       category: 1
     },
     {
       price: 500,
-      image_data: 'assets/appliances.jpg',
+      image_data: 'assets/suit.jpg',
       rating: 5,
       id: 4,
-      name: 'Home appliances',
-      quantity: 1,
+      name: 'Suit',
+      quantity: 20,
       category: 1
     },
     {
       price: 600,
-      image_data: 'assets/appliances.jpg',
+      image_data: 'assets/Blazer.jpg',
       rating: 3,
       id: 5,
-      name: 'Mobile',
-      quantity: 1,
+      name: 'Blazer and Chinos',
+      quantity: 50,
       category: 1
     },
     {
       price: 400,
-      image_data: 'assets/appliances.jpg',
+      image_data: 'assets/kurta.jpg',
       rating: 3,
       id: 6,
-      name: 'Appleproduct',
-      quantity: 1,
+      name: 'Kurta',
+      quantity: 100,
       category: 1
     },
     {
       price: 300,
-      image_data: 'assets/appliances.jpg',
+      image_data: 'assets/pyjama.jpg',
       rating: 3,
       id: 7,
-      name: 'Ac',
-      quantity: 1,
+      name: 'Pyjama',
+      quantity: 50,
+      category: 1
+    },
+    {
+      price: 300,
+      image_data: 'assets/sherwani.jpg',
+      rating: 3,
+      id: 7,
+      name: 'Sherwani',
+      quantity: 50,
+      category: 1
+    },
+    {
+      price: 300,
+      image_data: 'assets/wrap.jpg',
+      rating: 3,
+      id: 7,
+      name: 'Wrap dress',
+      quantity: 50,
+      category: 1
+    },
+    {
+      price: 300,
+      image_data: 'assets/peplum.jpg',
+      rating: 3,
+      id: 7,
+      name: 'Peplum dress',
+      quantity: 50,
+      category: 1
+    },
+    {
+      price: 300,
+      image_data: 'assets/shift.jpg',
+      rating: 3,
+      id: 7,
+      name: 'Shift dress',
+      quantity: 50,
       category: 1
     }
-  ]
-  displaylist:product[]=[]
-  autosuggestionlist:product[]=[]
-  row:number=3
-  currentpage:number =1
- searchvalue: string = ''
-  search(e: KeyboardEvent) {
+  ];
+  displaylist: product[] = [];
+  autosuggestionlist: product[] = [];
+  row: number = 6;
+  currentpage: number = 1;
+  searchvalue: string = '';
 
-    console.log(this.searchvalue);
-    this.displaylist = this.productlist.filter((data) => data.name.toLowerCase().includes(this.searchvalue.toLowerCase()))
-    if(this.searchvalue == ''){
-      this.autosuggestionlist =  []
-    }else{
-      this.autosuggestionlist =  this.productlist.filter((data) => data.name.toLowerCase().includes(this.searchvalue.toLowerCase()))    
+  autosuggestbox() {
+    if (this.searchvalue == '') {
+      this.autosuggestionlist = []
+    } else {
+      this.autosuggestionlist = this.productlist.filter((data) => data.name.toLowerCase().includes(this.searchvalue.toLowerCase()))
     }
-    
-
   }
+
+  // search() {
+
+  //   console.log(this.searchvalue);
+  //   this.displaylist = this.productlist.filter((data) => data.name.toLowerCase().includes(this.searchvalue.toLowerCase()))
+  //   if(this.searchvalue == ''){
+  //     this.autosuggestionlist =  []
+  //   }else{
+  //     this.autosuggestionlist =  this.productlist.filter((data) => data.name.toLowerCase().includes(this.searchvalue.toLowerCase()))    
+  //   }
+
+
+  // }
   assigninglist() {
     this.displaylist = JSON.parse(JSON.stringify(this.productlist));
   }
-  displaylist1(page:any){
-const start = this.row*(page-1);
-const end = start + this.row
-return this.displaylist.slice(start,end)
+  displaylist1(page: any) {
+    const start = this.row * (page - 1);
+    const end = start + this.row
+    return this.displaylist.slice(start, end)
   }
-  setpagination(){
-    const page_count = Math.ceil(8/this.row)
-    return Array.from({length:page_count},(_,i)=>i+1)
+  setpagination() {
+    const page_count = Math.ceil(8 / this.row)
+    return Array.from({ length: page_count }, (_, i) => i + 1)
   }
-  onpagechange(page:number){
+  onpagechange(page: number) {
     this.currentpage = page
   }
-  itemchose(name:string){
-this.searchvalue = name;
-this.displaylist = this.displaylist.filter((data) => data.name.toLowerCase().includes(this.searchvalue.toLowerCase()))
-this.autosuggestionlist = []
+  itemchose(name: string) {
+    this.searchvalue = name;
+    this.displaylist = this.displaylist.filter((data) => data.name.toLowerCase().includes(this.searchvalue.toLowerCase()))
+    this.autosuggestionlist = []
 
   }
 }
